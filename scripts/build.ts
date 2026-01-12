f96f607db6595bea74867cd9691498e4af752bd4
Phantozweb <iamsirenjeev@gmail.com>
2026-01-12 16:59:49 +0530

    feat: Update esbuild and configure Deno imports
    Updates the version of esbuild used in the project to 0.27.2.
    
    This commit also configures Deno's import map to use local `node_modules` for esbuild and the deno-loader, simplifying the dependency management for the build process.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,5 @@
-import * as esbuild from "npm:esbuild@0.20.2";
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.10.3";
+import * as esbuild from "esbuild";
+import { denoPlugins } from "@luca/esbuild-deno-loader";
 
 declare const Deno: any;
 
